e826f8976e2909303d641bd09d8feef76fd2e945
Roman <rm07ru@gmail.com>
2018-04-13 20:09:54 +0300

    Add UserInfo

---
+++ src/App/Home/UserInfo/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Icon, Image, Table, Grid } from 'semantic-ui-react';
+import moment from 'moment';
+
+import {DATE_FORMAT_M_D_Y} from '../../../settings';
+
+const UserInfo = props => {
+    return (
+        <Card>
+            <Image src={props.data.avatarUrl} />
+
+            <Card.Content>
+                <Card.Header>
+                    {props.data.login}
+                </Card.Header>
+
+                <Card.Meta>
+                    <span className='date'>
+                        Joined in {moment(props.data.createdAt).format(DATE_FORMAT_M_D_Y)}
+                    </span>
+                </Card.Meta>
+
+                <Card.Description>
+                    {props.data.bio}
+                </Card.Description>
+            </Card.Content>
+
+            <Card.Content extra>
+                <a>
+                    <Icon name='folder' />
+                    {props.data.repositories.totalCount} Repositories
+                </a>
+            </Card.Content>
+        </Card>
+    );
+};
+
+export default UserInfo;
