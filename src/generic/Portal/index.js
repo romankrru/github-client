66883d74da02bd7efcee0077c6c2be105955b9e9
Roman <rm07ru@gmail.com>
2018-04-13 20:08:40 +0300

    Add Protal

---
+++ src/generic/Portal/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import { compose, withProps, lifecycle } from 'recompact';
+
+const Portal = props => ReactDOM.createPortal(props.children, props.container);
+
+export default compose(
+    withProps({
+        rootSelector: document.body,
+        container: document.createElement('div'),
+    }),
+
+    lifecycle({
+        componentDidMount() {this.props.rootSelector.appendChild(this.props.container)},
+        componentWillUnmount() {this.props.rootSelector.removeChild(this.props.container)},
+    })
+)(Portal);
