17e0d406b1cda5873f8f64dd29978edf0ce71901
Roman <rm07ru@gmail.com>
2018-04-13 20:09:19 +0300

    Add Home component

---
+++ src/App/Home/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import { Grid } from 'semantic-ui-react';
+import {compose, branch, renderComponent} from 'recompact';
+
+import query from './gql/query';
+import UserInfo from './UserInfo';
+import RepositoriesTable from './RepositoriesTable';
+import Loader from '../../generic/Loader';
+
+const Home = props => (
+  <Grid>
+    <Grid.Row>
+      <Grid.Column width={4}>
+        <UserInfo data={props.data.viewer} />
+      </Grid.Column>
+
+      <Grid.Column width={12}>
+        <RepositoriesTable data={props.data.viewer.repositories.nodes}/>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+export default compose(
+  graphql(query),
+
+  branch(
+    props => props.data.loading,
+    renderComponent(Loader),
+  ),
+)(Home);
