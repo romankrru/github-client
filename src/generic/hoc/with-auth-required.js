0efc76df4cc01556a068a1819ee7bbfad063c82f
Roman <rm07ru@gmail.com>
2018-04-13 20:10:57 +0300

    Add  with-auth-required hoc

---
+++ src/generic/hoc/with-auth-required.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Redirect} from 'react-router';
+
+import {localStorageHelpers} from '../helpers';
+
+const withAuthRequired = BaseComponent => {
+  const WithAuthRequired = props => localStorageHelpers.load('AUTH_TOKEN')
+    ? <BaseComponent {...props} />
+    : <Redirect to="/auth" />;
+
+  return WithAuthRequired;
+};
+
+export default withAuthRequired;
