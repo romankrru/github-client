9ff1e52eb44daa9f648cce126569ed6fb0ebf591
Roman <rm07ru@gmail.com>
2018-04-23 22:59:40 +0300

    Add types to loader

--- src/generic/Loader/index.js
+++ src/generic/Loader/index.js
@@ -1,9 +1,11 @@
+// @flow
+
 import React from 'react';
 import { Loader } from 'semantic-ui-react';
 
 import Portal from '../Portal';
 
-const CustomLoader = props => (
+const CustomLoader = () => (
     <Portal>
         <Loader active />
     </Portal>
