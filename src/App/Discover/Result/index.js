4dd2f54552a067b35294c9123fd4a6de135afc9a
Roman <rm07ru@gmail.com>
2018-04-23 22:21:27 +0300

    Add types to Result

--- src/App/Discover/Result/index.js
+++ src/App/Discover/Result/index.js
@@ -1,10 +1,23 @@
+// @flow
+
 import React from 'react';
 import _ from 'lodash'
 import { Card, Icon } from 'semantic-ui-react'
 
 import styles from './assets/index.css';
 
-const Result = props => (
+const Result = (props: {
+    data: Array<{
+        node: {
+            id: string,
+            name: string,
+            description: ?string,
+            languages: {edges: Array<{node: {name: string}}>},
+            stargazers: {totalCount: number},
+            forks: {totalCount: number},
+        }
+    }>
+}) => (
     <Card.Group itemsPerRow={3}>
         {props.data.map(edge => (
             <Card key={edge.node.id}>
@@ -16,11 +29,11 @@ const Result = props => (
                         length: 90,
                     })}
                 />
-                
+
                 <Card.Content extra>
                     {edge.node.languages.edges[0].node.name} {' | '}
                     <Icon name='star' /> {edge.node.stargazers.totalCount} {' | '}
-                    <Icon name='fork' /> {edge.node.forks.totalCount}                                
+                    <Icon name='fork' /> {edge.node.forks.totalCount}
                 </Card.Content>
             </Card>
         ))}
