6597d92c6014c0b22a4542f69477f541b58dab10
Roman <rm07ru@gmail.com>
2018-04-30 00:53:02 +0300

    Add data to DetailsModal

--- src/App/Discover/DetailsModal/index.js
+++ src/App/Discover/DetailsModal/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { branch, renderNothing }  from 'recompact';
-import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+import { Button, Header, Icon, Modal, List, Table } from 'semantic-ui-react';
+import moment from 'moment';
+
+import { DATE_FORMAT_WITH_HOURS } from '../../../settings';
 
 const DetailsModal = props => {
 	return (
@@ -10,13 +13,42 @@ const DetailsModal = props => {
 			size='small'
 		>
 			<Header content={props.data.name} />
+
 			<Modal.Content>
 				<p>{props.data.description}</p>
+
+				<Table>
+					<Table.Body>
+						<Table.Row>
+							<Table.Cell>Forks</Table.Cell>
+							<Table.Cell>{props.data.forks.totalCount}</Table.Cell>							
+						</Table.Row>
+
+						<Table.Row>
+							<Table.Cell>Stars</Table.Cell>
+							<Table.Cell>{props.data.stargazers.totalCount}</Table.Cell>
+						</Table.Row>
+
+						<Table.Row>
+							<Table.Cell>Updated at</Table.Cell>
+							<Table.Cell>{moment(props.data.updatedAt).format(DATE_FORMAT_WITH_HOURS)}</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table>
+
+				<p>Languages:</p>
+
+				<List bulleted>
+					{props.data.languages.edges.map(lang => (
+						<List.Item key={lang.node.name}>{lang.node.name}</List.Item>					
+					))}
+				</List>
 			</Modal.Content>
+
 			<Modal.Actions>
-			<Button color='green' onClick={props.close} inverted>
-				<Icon name='checkmark' /> Got it
-				</Button>
+				<Button color='green' onClick={props.close} inverted>
+					<Icon name='checkmark' /> Got it
+					</Button>
 			</Modal.Actions>
 		</Modal>
 	)
--- src/settings/index.js
+++ src/settings/index.js
@@ -8,3 +8,4 @@ export const GITHUB_CLIENT_SECRET = '9d0d33a27761b0196c6fe502b760e3f43dc826a9';
 export const AUTH_URL_PATH = 'https://api.github.com/authorizations';
 
 export const DATE_FORMAT_M_D_Y = 'MMMM Do YYYY';
+export const DATE_FORMAT_WITH_HOURS = 'MMMM Do YYYY, h:mm:ss';
