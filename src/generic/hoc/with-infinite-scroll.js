4366da8d49a3f492fafec48f18847686d6f1eda1
Roman <rm07ru@gmail.com>
2018-05-09 14:39:00 +0300

    Fix linter in withInfiniteScroll hoc

--- src/generic/hoc/with-infinite-scroll.js
+++ src/generic/hoc/with-infinite-scroll.js
@@ -1,53 +1,46 @@
-/* eslint-disable */
+import _ from 'lodash';
+import { compose, withStateHandlers, lifecycle } from 'recompact';
 
-import {compose, withStateHandlers, lifecycle} from 'recompact';
-
-const withInfiniteScroll = (params) => compose(
+const withInfiniteScroll = params => compose(
     withStateHandlers(
-        {isFetchMoreLoading: false},
-        {setIsFetchMoreLoading: () => value => ({isFetchMoreLoading: value})}
+        { isFetchMoreLoading: false },
+        { setIsFetchMoreLoading: () => value => ({ isFetchMoreLoading: value }) },
     ),
 
     lifecycle({
         componentDidMount() {
-             const onDocumentScroll = _.throttle(() => {
-                const documentHeight = document.body.scrollHeight
-                const screenHeight = document.body.clientHeight
-                const scrolledHeight = window.pageYOffset
+            const onDocumentScroll = _.throttle(() => {
+                const documentHeight = document.body.scrollHeight;
+                const screenHeight = document.body.clientHeight;
+                const scrolledHeight = window.pageYOffset;
 
-                if (documentHeight - screenHeight - scrolledHeight <  50) {
-                    if (this.props.isFetchMoreLoading)
-                        return;
-                        
+                if (documentHeight - screenHeight - scrolledHeight < 50) {
+                    if (this.props.isFetchMoreLoading) { return; }
+
                     this.props.setIsFetchMoreLoading(true);
-                    
-                    window.scrollBy({
-                        behavior: "smooth",
-                        left: 0,
-                        top: 100
-                    });
 
-                    // const cursor = _.last(this.props.data.search.edges).cursor  
+                    window.scrollBy({
+                        behavior: 'smooth',
+                        left: 0,
+                        top: 100,
+                    });
 
                     this.props.data.fetchMore({
                         query: params.query,
                         variables: params.variables(this.props),
-                        
-                        updateQuery: (prevResult, newResult) => {
-                            // this.props.setIsFetchMoreLoading(false);
 
-                            if (_.isEmpty(newResult.fetchMoreResult))
-                                return prevResult;
+                        updateQuery: (prevResult, newResult) => {
+                            if (_.isEmpty(newResult.fetchMoreResult)) { return prevResult; }
 
                             return params.update(prevResult, newResult);
-                        }
-                    }).then(() => this.props.setIsFetchMoreLoading(false))
+                        },
+                    }).then(() => this.props.setIsFetchMoreLoading(false));
                 }
             }, 100);
 
-            document.addEventListener('scroll', onDocumentScroll)
-        }
+            document.addEventListener('scroll', onDocumentScroll);
+        },
     }),
-)
+);
 
 export default withInfiniteScroll;
