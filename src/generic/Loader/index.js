41acb59c3498fd0907240b8c741477eea8846135
romankrru <rm07ru@gmail.com>
2019-02-22 01:56:03 +0300

    Fix code formatting

--- src/generic/Loader/index.js
+++ src/generic/Loader/index.js
@@ -1,13 +1,13 @@
 // @flow
 import React from 'react';
-import { Loader } from 'semantic-ui-react';
+import {Loader} from 'semantic-ui-react';
 
 import Portal from '../Portal';
 
 const CustomLoader = () => (
-    <Portal>
-        <Loader active />
-    </Portal>
+	<Portal>
+		<Loader active />
+	</Portal>
 );
 
 export default CustomLoader;
