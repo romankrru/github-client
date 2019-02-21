cc0f6cf46624cf67619db9a6309e442238ade144
romankrru <rm07ru@gmail.com>
2019-02-22 00:44:30 +0300

    Remove ApolloProvider

--- src/App/index.js
+++ src/App/index.js
@@ -1,50 +1,46 @@
 // @flow
 import React from 'react';
-import { ApolloClient } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloLink, concat } from 'apollo-link';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {ApolloLink, concat} from 'apollo-link';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import {ApolloProvider} from 'react-apollo-hooks';
 
-import { localStorageHelpers } from '../generic/helpers';
+import {localStorageHelpers} from '../generic/helpers';
 import Layout from '../generic/Layout';
-import { withAuthRequired } from '../generic/hoc';
+import {withAuthRequired} from '../generic/hoc';
 import Home from './Home';
 import Discover from './Discover';
 
-const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' });
+const httpLink = new HttpLink({uri: 'https://api.github.com/graphql'});
 
 // add the authorization to the headers
 const authMiddleware = new ApolloLink((operation, forward) => {
-    operation.setContext({
-        headers: {
-            authorization: `Bearer ${localStorageHelpers.load('AUTH_TOKEN')}`,
-        },
-    });
+	operation.setContext({
+		headers: {
+			authorization: `Bearer ${localStorageHelpers.load('AUTH_TOKEN')}`,
+		},
+	});
 
-    return forward(operation);
+	return forward(operation);
 });
 
 const client = new ApolloClient({
-    link: concat(authMiddleware, httpLink),
-    cache: new InMemoryCache(),
+	link: concat(authMiddleware, httpLink),
+	cache: new InMemoryCache(),
 });
 
 const App = () => (
-    // FIXME: remove ApolloProvider later
-    <ApolloProvider client={client}>
-        <ApolloHooksProvider client={client}>
-            <Layout>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/discover" component={Discover} />
-                    <Redirect to="/" />
-                </Switch>
-            </Layout>
-        </ApolloHooksProvider>
-    </ApolloProvider>
+	<ApolloProvider client={client}>
+		<Layout>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/discover" component={Discover} />
+				<Redirect to="/" />
+			</Switch>
+		</Layout>
+	</ApolloProvider>
 );
 
 export default withAuthRequired(App);
