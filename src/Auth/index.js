ba9861a9bbc991084e307351f640258797fc01bb
romankrru <rm07ru@gmail.com>
2019-02-22 02:19:06 +0300

    Move `getToken` into separate function

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -14,8 +14,36 @@ import {useNotification} from '../generic/NotificationManager';
 import {localStorageHelpers} from '../generic/helpers';
 import styles from './assets/index.module.css';
 
+const getToken = (data: {
+	login: string,
+	password: string,
+}): Promise<string | void> => {
+	const bytes = `${data.login.trim()}:${data.password.trim()}`;
+	const encoded = base64.encode(bytes);
+
+	return fetch(AUTH_URL_PATH, {
+		method: 'POST',
+
+		headers: {
+			Authorization: `Basic ${encoded}`,
+			'User-Agent': 'GitHub Issue Browser',
+			'Content-Type': 'application/json; charset=utf-8',
+			Accept: 'application/vnd.github.inertia-preview+json',
+		},
+
+		body: JSON.stringify({
+			client_id: GITHUB_CLIENT_ID,
+			client_secret: GITHUB_CLIENT_SECRET,
+			scopes: ['user', 'repo', 'read:org'],
+			note: 'not abuse',
+		}),
+	})
+		.then(res => res.json())
+		.then(json => json.token);
+};
+
 const Auth = (props: $Exact<{history: RouterHistory}>) => {
-	const notificationDispatcher = useNotification();
+	const notification = useNotification();
 	const [formState, setFormState] = useState({login: '', password: ''});
 	const [isValid, setIsValid] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
@@ -30,46 +58,33 @@ const Auth = (props: $Exact<{history: RouterHistory}>) => {
 		});
 
 	const signIn = () => {
-		const bytes = `${formState.login.trim()}:${formState.password.trim()}`;
-		const encoded = base64.encode(bytes);
-
 		setIsLoading(true);
 
-		return fetch(AUTH_URL_PATH, {
-			method: 'POST',
-
-			headers: {
-				Authorization: `Basic ${encoded}`,
-				'User-Agent': 'GitHub Issue Browser',
-				'Content-Type': 'application/json; charset=utf-8',
-				Accept: 'application/vnd.github.inertia-preview+json',
-			},
-
-			body: JSON.stringify({
-				client_id: GITHUB_CLIENT_ID,
-				client_secret: GITHUB_CLIENT_SECRET,
-				scopes: ['user', 'repo', 'read:org'],
-				note: 'not abuse',
-			}),
-		})
-			.then(res => res.json())
-
-			.then(json => {
-				setIsLoading(false);
-
-				if (!json.token) {
+		getToken({login: formState.login, password: formState.password})
+			.then(token => {
+				if (!token) {
 					setFormState({...formState, password: ''});
 
-					notificationDispatcher.error({
+					notification.error({
 						message: 'Invalid password or email.',
 					});
 
 					return;
 				}
 
-				localStorageHelpers.save('AUTH_TOKEN', json.token);
+				localStorageHelpers.save('AUTH_TOKEN', token);
 				props.history.replace('/');
-			});
+			})
+
+			.catch(err => {
+				console.error(err);
+
+				notification.error({
+					message: 'Some error occurred. Please try again.',
+				});
+			})
+
+			.finally(() => setIsLoading(false));
 	};
 
 	useEffect(() => {
